b27581c38e5a4d77f1e109ff9e94c91929369376
austin2118ace <austin2118ace@gmail.com>
2025-07-28 21:49:35 -0400

    import and call the default_setup; log the resultant directory

--- bpod_rig/__main__.py
+++ bpod_rig/__main__.py
@@ -1,11 +1,18 @@
 """main entry point for bpod-rig"""
+
 import logging
+
+from bpod_rig.IO import default_setup
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+
 def main():
     logger.info("Initializing bpod-rig!")
+    bpod_directory = default_setup.create_default_directories()
+    logger.info("Bpod directory initialized to %s", bpod_directory)
+
 
 if __name__ == "__main__":
     main()
