cfd140d129aff4b8fea8bf3fd1fe802bd51d9760
austin2118ace <austin2118ace@gmail.com>
2025-07-28 23:00:28 -0400

    call default copy from the main function

--- bpod_rig/__main__.py
+++ bpod_rig/__main__.py
@@ -11,6 +11,7 @@ logger = logging.getLogger(__name__)
 def main():
     logger.info("Initializing bpod-rig!")
     bpod_directory = default_setup.create_default_directories()
+    default_setup.copy_default_files(bpod_directory)
     logger.info("Bpod directory initialized to %s", bpod_directory)
 
 
