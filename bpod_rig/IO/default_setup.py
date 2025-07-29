ad1ac5356406832fcb7dbc0243115dfeb12c5d51
austin2118ace <austin2118ace@gmail.com>
2025-07-28 22:56:51 -0400

    Import shutil and the example files directory Copy the included default settings/calibration json files to the appropriate directories If desired, only copy if the destination directories are empty

--- bpod_rig/IO/default_setup.py
+++ bpod_rig/IO/default_setup.py
@@ -1,8 +1,11 @@
 """Module to create the default Bpod directory and associated subdirs"""
 
 import logging
+import shutil
 
 from pathlib import Path
+
+from examples import calibration, settings
 
 DEFAULT_SUBDIRS = ["Calibrations", "Data", "Protocols", "Settings"]
 DEFAULT_DIR_NAME = "Bpod"
@@ -40,3 +43,48 @@ def create_default_directories(default_path_override: Path = None) -> Path:
             _path.mkdir(parents=True, exist_ok=True)
 
     return bpod_folder_path
+
+def copy_default_files(bpod_dir: Path, only_if_empty:bool = True):
+    """
+    Function to copy default settings and calibration .json files from the included
+    examples package
+    :param bpod_dir: (pathlib.Path): Path to the Bpod directory
+    :param only_if_empty: (bool) (Optional): Only copy the files if the destination
+     directory is empty
+    :return:
+    """
+    calibration_example_dir = Path(calibration.__path__[0])
+    settings_example_dir = Path(settings.__path__[0])
+    default_calibration_files = calibration_example_dir.glob("*.json")
+    default_settings_files = settings_example_dir.glob("*.json")
+
+    bpod_calibration_dir = bpod_dir.joinpath("Calibrations")
+    bpod_settings_dir = bpod_dir.joinpath("Settings")
+    bpod_calibration_dir_contents = list(bpod_calibration_dir.iterdir())
+    bpod_settings_dir_contents = list(bpod_settings_dir.iterdir())
+
+
+    if (
+        (only_if_empty and len(bpod_calibration_dir_contents) == 0) or
+        not only_if_empty
+    ):
+        # If only_if_empty is true, the calibration dir must be empty; if only_if_empty
+        # is false, we'll copy no matter what
+        for cal_file in default_calibration_files:
+            try:
+                logger.debug("Copying %s to %s...", cal_file, bpod_calibration_dir)
+                shutil.copy2(cal_file, bpod_calibration_dir)
+            except Exception as e:
+                logger.error("Error copying %s! Original Exception: %s", cal_file, e)
+
+    if (
+        (only_if_empty and len(bpod_settings_dir_contents) == 0) or
+        not only_if_empty
+    ):
+        for setting_file in default_settings_files:
+            try:
+                logger.debug("Copying %s to %s...", setting_file, bpod_settings_dir)
+                shutil.copy2(setting_file, bpod_settings_dir)
+            except Exception as e:
+                logger.error("Error copying %s! Original Exception: %s", setting_file, e)
+
