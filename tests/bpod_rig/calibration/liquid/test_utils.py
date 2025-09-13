87e9fa79159bcb8b6cdbdf4bd1a0cf9374b2a8ef
George Stuyt <15712782+ogeesan@users.noreply.github.com>
2025-09-14 01:19:51 +1000

    Add more tests

--- tests/bpod_rig/calibration/liquid/test_utils.py
+++ tests/bpod_rig/calibration/liquid/test_utils.py
@@ -1,7 +1,8 @@
+import datetime
 import unittest
 
 from bpod_rig.calibration.liquid import utils
-from bpod_rig.calibration.liquid.models import ValveData
+from bpod_rig.calibration.liquid.models import ValveData, ValveDataManager
 
 
 class TestSuggestDuration(unittest.TestCase):
@@ -24,3 +25,41 @@ class TestSuggestDuration(unittest.TestCase):
         self.valve.add_measurement(22, 2)
         self.valve.add_measurement(66, 9.5)
         self.assertAlmostEqual(self.suggest_duration(), 44.0, places=3)
+
+class TestCheckValveManagerUserUpdate(unittest.TestCase):
+    def setUp(self):
+        self.manager = ValveDataManager()
+
+    def test_earlier(self):
+        self.manager.metadata.modification_datetime = datetime.datetime(1999, 1, 1)
+        self.assertFalse(utils.check_valvemanager_user_updated(self.manager))
+
+    def test_later(self):
+        self.manager.create_valve("Valve1")
+        self.manager.get_valve("Valve1").add_measurement(15, 20)
+        self.assertTrue(utils.check_valvemanager_user_updated(self.manager))
+
+    def test_equal(self):
+        self.manager.metadata.modification_datetime = datetime.datetime(2000, 1, 1)
+        self.assertFalse(utils.check_valvemanager_user_updated(self.manager))
+
+    def test_dummy(self):
+        from bpod_rig.calibration.liquid.populate_examples import create_default_json
+
+        manager = ValveDataManager.model_validate_json(create_default_json())
+        self.assertFalse(utils.check_valvemanager_user_updated(manager))
+
+class TestCheckCOM(unittest.TestCase):
+    def setUp(self):
+        self.manager = ValveDataManager()
+        self.manager.metadata.COM = 'COM3'
+
+    def test_matching(self):
+        self.assertEqual(utils.check_COM(self.manager, "COM3"), "yes")
+
+    def test_no_match(self):
+        self.assertEqual(utils.check_COM(self.manager, "COM4"), "no")
+
+    def test_unknown(self):
+        self.manager.metadata.COM = ""
+        self.assertEqual(utils.check_COM(self.manager, "COM3"), "unknown")
