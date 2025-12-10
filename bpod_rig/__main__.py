3ce434fa261eafb90165d5e3f3917515a0e5e1ed
Austin Pauley <austin2118ace@gmail.com>
2025-12-10 09:09:15 -0500

    __main__: remove unused arguments for create default folder structure

--- bpod_rig/__main__.py
+++ bpod_rig/__main__.py
@@ -15,10 +15,10 @@ def main():
     logger.info("Initializing bpod-rig!")
     # Setup folders. "EMU" is the USB serial port argument for the emulator.
     # create_default_directories() is called later to add folders for each FSM on the PC
-    bpod_directory = default_setup.create_default_directories("EMU")
+    bpod_directory = default_setup.create_default_directories()
 
     # Populate this machine's folders with the default config and calibration files.
-    default_setup.copy_default_files(bpod_directory, "EMU")
+    default_setup.copy_default_files(bpod_directory)
 
     inital_system_config = utils.init_system_configuration(
         bpod_directory
