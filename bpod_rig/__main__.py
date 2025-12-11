a1be0da09ced9069d86984322c738afff434853c
Austin Pauley <austin2118ace@gmail.com>
2025-12-10 21:40:11 -0500

    __main__: Recreate default folder structure based on user input

--- bpod_rig/__main__.py
+++ bpod_rig/__main__.py
@@ -5,7 +5,7 @@ from pydantic import ValidationError
 
 from bpod_rig.config import utils
 from bpod_rig.config.system_settings import SystemPaths
-from bpod_rig.IO import default_setup
+from bpod_rig.IO import default_setup, cli_io
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -26,13 +26,28 @@ def main():
         logger.error("Error initializing system paths: %s", exc_info=ve)
 
     if not bpod_dir_verified:
-        logger.info("The Bpod directory at %s failed to verify!"
-                    " This could be due to a partially initialized folder structure or "
-                    "improper file path!", bpod_directory)
-        response = input(f"Would you like to re-create the default folder structure at {bpod_directory}? (y/n)")
+        logger.info(
+            "The Bpod directory at %s failed to verify!"
+            " This could be due to a partially initialized folder structure or "
+            "improper file path!",
+            bpod_directory,
+        )
+        response = cli_io.yes_no_prompt(
+            f"Would you like to (re)initialize {bpod_directory} as the base"
+            f" Bpod directory?"
+        )
 
-        if response == "y":
+        if response:
             bpod_directory = default_setup.create_default_directories()
+            try:
+                system_paths = SystemPaths(base_dir=bpod_directory)
+                bpod_dir_verified = True
+                logging.debug("System paths at %s verified", bpod_directory)
+            except ValidationError as ve:
+                logger.error("Error initializing system paths: %s", exc_info=ve)
+        else:
+            logger.info("No valid Bpod directory! Shutting down.")
+            return
 
     # Populate this machine's folders with the default config and calibration files.
     default_setup.copy_default_files(bpod_directory)
