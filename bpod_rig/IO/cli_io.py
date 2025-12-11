2e06cc434b5b36c8e0e1bb8490cd216908cf5a33
Austin Pauley <austin2118ace@gmail.com>
2025-12-11 16:14:34 -0500

    cli_io: somehow used a function that doesn't exist

--- bpod_rig/IO/cli_io.py
+++ bpod_rig/IO/cli_io.py
@@ -4,7 +4,7 @@ def yes_no_prompt(prompt: str) -> bool:
     response = ''
 
     while response not in ['y', 'n', 'Y', 'N']:
-        response = click.input(prompt, prompt_suffix=' [y/N] ')
+        response = click.prompt(prompt, prompt_suffix=' [y/N] ')
 
     if response in ['y', 'Y']:
         return True
