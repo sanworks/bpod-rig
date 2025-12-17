7c500acc81a025c9a3a5520b1753241593e712c1
Austin Pauley <austin2118ace@gmail.com>
2025-12-17 10:33:02 -0500

    cli_io: prompt user for path with some error checking

--- bpod_rig/IO/cli_io.py
+++ bpod_rig/IO/cli_io.py
@@ -1,4 +1,9 @@
+import logging
+from pathlib import Path
+
 import click
+
+logger = logging.getLogger(__name__)
 
 def yes_no_prompt(prompt: str) -> bool:
     response = ''
@@ -10,3 +15,21 @@ def yes_no_prompt(prompt: str) -> bool:
         return True
     else:
         return False
+
+def prompt_for_path(prompt: str) -> Path:
+    validated = False
+    path = []
+
+    while not validated:
+        unverified_path = click.prompt(prompt)
+        response = yes_no_prompt(f"Is {unverified_path} the correct path?")
+        if response:
+            try:
+                path = Path(unverified_path)
+                validated = True
+            except Exception as e:
+                logger.error("Invalid path: %s", exc_info=e)
+                click.echo("Invalid path entered, please try again.", err=True)
+
+    return path
+
