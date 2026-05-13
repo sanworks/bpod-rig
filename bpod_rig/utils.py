81c57b794e1a2bd5dd9c9c67d2ce2881d67e6afd
Austin Pauley <austin2118ace@gmail.com>
2026-05-13 14:27:40 -0400

    refactor get_default_params to get_func_params to enable to return of 'required' parameters

--- bpod_rig/utils.py
+++ bpod_rig/utils.py
@@ -2,20 +2,29 @@
 import inspect
 from typing import Callable, Any
 
-def get_default_params(*functions: Callable) -> dict[str, Any]:
-    """This function takes one or more functions and returns a dictionary of default parameters.
+def get_func_params(*functions: Callable, optional_only: bool = False) -> dict[str, Any]:
+    """This function takes one or more functions and returns a dictionary parameters.
 
-    Knowing which parameters in a function accept default arguments is useful
-    for **kwargs parsing. This function uses inspect to find the parameters in a function's
-    signature and adds those with default values to a dictionary.
+    Knowing the keyword-based parameters for a function is useful for **kwargs parsing.
+    This function uses inspect to retrieve the parameters in a function's signature and adds
+    to a dictionary. This function can also filter for parameters that have default values
+    and are optional.
 
-    The 'self' parameter and required parameters are not returned
+    The 'self' parameter is not returned
 
     Example function signature:
         def func1(self, param1, param2 = 'foo', param3 = 'baz')
 
     The resultant dictionary from get_default_params(func1) will be:
     {
+        'required': ['param1'],
+        'param2': 'foo',
+        'param3': 'baz',
+    }
+
+    The resultant dictionary from get_default_params(func1, optional_only) will be:
+    {
+        'required': [],
         'param2': 'foo',
         'param3': 'baz',
     }
@@ -25,19 +34,33 @@ def get_default_params(*functions: Callable) -> dict[str, Any]:
     functions : *Callable
         One or more functions that accept default arguments
 
+    optional_only : bool (optional)
+        Only return parameters with default values defined in the signature
+
     Returns
     -------
     dict[str, Any]
-        Dictionary of parameters with default values and those values for all
-        functions
+        Dictionary of parameters
 
+        If the parameter is required, it is stored in a list with key 'required'
+        If the parameter is optional, it is stored as param_name : default_value
     """
-    return {
-        param: value.default
-        for func in functions for param, value in inspect.signature(func).parameters.items()
-        if value is not inspect.Parameter.empty and
-        param != "self"
+    params = {
+        'required': [],
     }
+
+    for func in functions:
+        sig = inspect.signature(func)
+        for param, value in sig.parameters.items():
+            if value.default == inspect.Parameter.empty:
+                if optional_only:
+                    continue
+                params['required'].append(param)
+
+            if param != "self":
+                params[param] = value.default
+
+    return params
 
 
 
