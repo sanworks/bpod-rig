a9b4391a400786dbfa3e8ccdf07be6abfd820cfa
Austin Pauley <austin2118ace@gmail.com>
2026-05-13 16:24:39 -0400

    skeleton of BpodSubject class

---
+++ bpod_rig/protocols/subject.py
@@ -0,0 +1,15 @@
+import datetime
+
+from bpod_rig.session import BpodSession
+
+# TODO: make this a Pydantic model
+class BpodSubject:
+    def __init__(self):
+        self.id: str | None = None
+        self.species: str | None = None
+        self.DOB: datetime.date | None = None
+        self.cohort: str | None = None
+        self.genotype: str | None = None
+        self.description: str | None = None
+
+        self.current_session: BpodSession | None = None
