391beb1b8229d695d4d073256d0283f9ba7bf3cd
Austin Pauley <austin2118ace@gmail.com>
2026-05-13 14:28:37 -0400

    BpodSession connect function to handle connecting to a specific Bpod

--- bpod_rig/session/bpod_session.py
+++ bpod_rig/session/bpod_session.py
@@ -1,13 +1,22 @@
+import logging
 from datetime import datetime
+from inspect import signature
 from pathlib import Path
+from serial import SerialException
 
-from bpod_core.bpod import Bpod, RemoteBpod
+from bpod_core.bpod import Bpod, RemoteBpod, BpodError
 
 from bpod_rig.config.system_settings import SystemSettings
+from bpod_rig.protocols.bpod_protocol import BpodProtocol
+from bpod_rig.utils import get_func_params
 
+logger = logging.getLogger(__name__)
 
 class BpodSession:
     def __init__(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+
         self.bpod: Bpod | RemoteBpod | None = None
         self.system_settings: SystemSettings | None = None
         self.session_params: dict | None = None
@@ -22,10 +31,33 @@ class BpodSession:
         # Session Information
         self.session_id: str | None = None
         self.subject: None = None
-        self.protocol: None = None
+        self.protocol: BpodProtocol |  None = None
         self.system_info: dict | None = None
 
         self.session_dir: Path | None = None
+
+    def connect(self):
+        if self.bpod:
+            raise ConnectionError("Bpod already connected!")
+        else:
+            connection_args = dict()
+
+            args = get_func_params(Bpod.__init__, RemoteBpod.__init__)
+            # Get the possible parameters for the Bpod/RemoteBpod constructors
+
+            for arg in args:
+                if arg in self._kwargs:
+                    logger.debug("Bpod connection argument %s found in BpodSession kwargs", args)
+                    connection_args[arg] = self._kwargs[arg]
+
+            try:
+                self.bpod = Bpod(**connection_args)
+            except SerialException as se:
+                logger.error("Unable to open serial connection to Bpod!", exc_info=se)
+            except BpodError as bpe:
+                logger.error("Handshake with Bpod failed!", exc_info=bpe)
+
+
 
     def start(self):
         pass
