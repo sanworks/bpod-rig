917c2d55bea6a7a63ff54f9041627735eb83afa7
Austin Pauley <austin2118ace@gmail.com>
2026-05-16 15:30:17 -0400

    optionally pass a protocol function handle to the class constructor to enable both class-based protocols and function-based protocols

--- bpod_rig/protocols/protocol.py
+++ bpod_rig/protocols/protocol.py
@@ -1,3 +1,6 @@
+from typing import Callable
+from types import MethodType
+
 from bpod_core.bpod import Bpod, RemoteBpod
 from bpod_rig.session.bpod_session import BpodSession
 
@@ -7,22 +10,33 @@ class BpodProtocol:
     version: str = "0"
     default_config: dict = dict()
 
-    def __init__(self, bpod_session: BpodSession):
+    def __init__(self, bpod_session: BpodSession, protocol_function: Callable | None = None):
         self.session: BpodSession = bpod_session
         self.end_session: bool = False
 
         self.protocol_name: str | None = None
         self.protocol_version: str | None = None
 
+        self._protocol_function: Callable | None = protocol_function
+
+        if self._protocol_function:
+            # If the user writes a "simple" protocol.py, a handle to the function
+            # can be passed to BpodProtocol to override the self.protocol function
+            self.protocol = MethodType(self._protocol_function, self.session)
 
     def setup(self):
         pass
 
-    def run(self):
+    def protocol(self):
         raise NotImplementedError("You must implement your Bpod protocol within the run function!")
 
     def teardown(self):
         pass
+
+    def run(self):
+        self.setup()
+        self.protocol()
+        self.teardown()
 
     @property
     def bpod(self) -> Bpod | RemoteBpod:
