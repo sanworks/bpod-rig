70a25ac8aaaf48a4938919bba6fb7acd04a2d409
Austin Pauley <austin2118ace@gmail.com>
2026-05-17 19:25:02 -0400

    set_protocol function for BpodSession | logging

--- bpod_rig/session/bpod_session.py
+++ bpod_rig/session/bpod_session.py
@@ -1,20 +1,22 @@
+from __future__ import annotations
 import logging
 import warnings
 from datetime import datetime
 from pathlib import Path
+from typing import TYPE_CHECKING, Callable
 
 from bpod_core.bpod import Bpod, BpodError, RemoteBpod
 from serial import SerialException
 
 from bpod_rig.config.system_settings import SystemSettings
-from bpod_rig.protocols import BpodProtocol
 from bpod_rig.utils import get_func_params
 
-logger = logging.getLogger(__name__)
-
+if TYPE_CHECKING:
+    from bpod_rig.protocols import BpodProtocol
 
 class BpodSession:
     def __init__(self, *args, **kwargs):
+        self.logger = logging.getLogger(__name__)
         self._args = args
         self._kwargs = kwargs
 
@@ -37,8 +39,24 @@ class BpodSession:
 
         self.session_dir: Path | None = None
 
+
+    def set_protocol(self, protocol: BpodProtocol | Callable):
+        from bpod_rig.protocols import BpodProtocol
+        # import this here to avoid circular dependencies
+        if isinstance(protocol, type) and issubclass(protocol, BpodProtocol):
+            self.logger.debug("Setting protocol to BpodProtocol object!")
+            self.protocol = protocol
+        elif isinstance(protocol, Callable):
+            self.logger.debug("Setting protocol to Callable object!")
+            bpod_protocol = BpodProtocol(self, protocol_function=protocol)
+            self.protocol = bpod_protocol
+        else:
+            raise TypeError("Invalid protocol object or function passed!")
+
+
+
     def start(self, warn: bool = True):
-        logger.info("Starting Bpod Session!")
+        self.logger.info("Starting Bpod Session!")
         if warn:
             warnings.warn(
                 "BpodSession started outside of a context manager."
@@ -49,22 +67,29 @@ class BpodSession:
 
         try:
             self._connect()
+            self.logger.debug("Successfully connected to Bpod!")
         except SerialException as se:
-            logger.error("Unable to open serial connection to Bpod!", exc_info=se)
+            self.logger.error("Unable to open serial connection to Bpod!", exc_info=se)
         except BpodError as bpe:
-            logger.error("Handshake with Bpod failed!", exc_info=bpe)
+            self.logger.error("Handshake with Bpod failed!", exc_info=bpe)
+
+        self.session_start_time = datetime.now()
+
 
     def end(self):
-        logger.info("Closing Bpod Session!")
+        self.logger.info("Closing Bpod Session!")
 
         try:
             self._disconnect()
         except SerialException as se:
-            logger.error("Unable to close serial connection to Bpod!", exc_info=se)
+            self.logger.error("Unable to close serial connection to Bpod!", exc_info=se)
+
+        self.session_end_time = datetime.now()
+
 
     def _connect(self):
         if self.bpod:
-            logger.debug("Bpod already connected!")
+            self.logger.debug("Bpod already connected!")
             return
 
         connection_args = {}
@@ -82,19 +107,27 @@ class BpodSession:
 
         for arg in potential_args:
             if arg in self._kwargs:
-                logger.debug(
+                self.logger.debug(
                     "Bpod connection argument %s found in BpodSession kwargs", potential_args
                 )
                 connection_args[arg] = self._kwargs[arg]
+        self.logger.debug("Attempting to connect to Bpod")
         self.bpod = Bpod(**connection_args)
+
 
     def _disconnect(self):
         if not self.bpod:
             raise ConnectionError("Bpod is already disconnected!")
         self.bpod.close()
 
+
     def __enter__(self):
+        self.logger.debug("Enter context manager!")
         self.start(warn=False)
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        self.logger.debug("Exiting context manager!")
         self.end()
+        if exc_type is not None:
+            self.logger.error("Error!", exc_info=(exc_type, exc_val, exc_tb))
