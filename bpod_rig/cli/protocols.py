75d8a03671652bbdbd6aedefe60e943b05df4500
ogeesan <george.stuyt@gmail.com>
2026-05-29 22:01:22 +1000

    Fix checks

--- bpod_rig/cli/protocols.py
+++ bpod_rig/cli/protocols.py
@@ -4,7 +4,10 @@ import typer
 
 from bpod_rig.config import get_settings
 
-app = typer.Typer(help="Manage protocols", no_args_is_help=True,)
+app = typer.Typer(
+    help="Manage protocols",
+    no_args_is_help=True,
+)
 
 
 @app.command(name="list")
@@ -13,14 +16,15 @@ def list_protocols():
     _ = get_settings()
     raise NotImplementedError("Protocol searcher required.")
 
+
 @app.command()
-def open(
+def open(  # noqa: A001
     protocol: Annotated[str | None, typer.Option()] = None,
     open_file: Annotated[bool, typer.Option()] = False,
-    ):
-    """Open a protoocl folder (or file)"""
+):
+    """Open a protoocl folder (or file)."""
     raise NotImplementedError("Protocol searcher required.")
-    
+
 
 if __name__ == "__main__":
     app()
