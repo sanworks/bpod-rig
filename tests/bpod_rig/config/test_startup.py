bec9c0b98c2ab3038f7b6cf54be92faf0329d4fb
ogeesan <george.stuyt@gmail.com>
2026-06-16 22:49:45 +1000

    Use function fixture

--- tests/bpod_rig/config/test_startup.py
+++ tests/bpod_rig/config/test_startup.py
@@ -1,8 +1,9 @@
 import logging
 import tempfile
+from dataclasses import dataclass
 from logging.config import dictConfig
 from pathlib import Path
-from typing import cast
+from typing import Generator, cast
 
 import pytest
 
@@ -18,6 +19,36 @@ def get_logger() -> BpodLogger:
     return logger
 
 
+@dataclass
+class TempSetup:
+    temp_dir: tempfile.TemporaryDirectory
+    temp_path: Path
+    bpod_path: Path
+    system_path: Path
+
+
+@pytest.fixture
+def temp_setup(monkeypatch: pytest.MonkeyPatch) -> Generator[TempSetup, None, None]:
+    """Fixture to create temporary directories for testing."""
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_path = Path(temp_dir.name)
+    bpod_path = temp_path / "Bpod"
+    system_path = temp_path / "sanworks"
+    monkeypatch.setattr(startup, "SYSTEM_CONFIG_DIR", system_path)
+    monkeypatch.setattr(
+        startup, "SYSTEM_CONFIG_FILE", system_path.joinpath("config.json")
+    )
+
+    yield TempSetup(
+        temp_dir=temp_dir,
+        temp_path=temp_path,
+        bpod_path=bpod_path,
+        system_path=system_path,
+    )
+
+    temp_dir.cleanup()
+
+
 class DefaultChoiceAdapter(startup.StartupChoiceProtocol):
     def choose_override_path_first_init(self, default_path: Path) -> Path:
         return default_path
@@ -30,78 +61,50 @@ class DefaultChoiceAdapter(startup.StartupChoiceProtocol):
 
 
 class TestCreateDefaultDirectories:
-    @pytest.fixture(autouse=True)
-    def setup_and_teardown(self):
-        """Setup and teardown."""
-        self.temp_dir = tempfile.TemporaryDirectory()
-        self.temp_path = Path(self.temp_dir.name)
-        self.bpod_path = self.temp_path / "Bpod"
 
-        yield  # test runs here
-
-        self.temp_dir.cleanup()
-
-    def test_folder_creation(self):
-        startup.create_default_directories(self.bpod_path)
-        assert self.bpod_path.exists()
+    def test_folder_creation(self, temp_setup: TempSetup):
+        startup.create_default_directories(temp_setup.bpod_path)
+        assert temp_setup.bpod_path.exists()
 
 
 class TestInitService:
-    @pytest.fixture(autouse=True)
-    def setup_and_teardown(self, monkeypatch: pytest.MonkeyPatch):
-        """Setup and teardown."""
-        self.temp_dir = tempfile.TemporaryDirectory()
-        self.temp_path = Path(self.temp_dir.name)
 
-        # Set DEFAULT_BPOD_PATH and SYSTEM_CONFIG_DIR to temp paths for testing
-        self.bpod_path = self.temp_path / "Bpod"
-        self.system_path = self.temp_path / "sanworks"
-
-        monkeypatch.setattr(startup, "SYSTEM_CONFIG_DIR", self.system_path)
-        monkeypatch.setattr(
-            startup, "SYSTEM_CONFIG_FILE", self.system_path.joinpath("config.json")
-        )
-
-        yield  # test runs here
-
-        self.temp_dir.cleanup()
-
-    def test_happy_path(self):
+    def test_happy_path(self, temp_setup: TempSetup):
         result = startup.initialize_bpod_system(
             choices=DefaultChoiceAdapter(),
-            default_bpod_path=self.bpod_path,
+            default_bpod_path=temp_setup.bpod_path,
             logger=get_logger(),
         )
 
         expected_result = startup.InitResult(
             state=startup.InitState.COMPLETED,
             message="Initialization successful.",
-            bpod_path=self.bpod_path,
+            bpod_path=temp_setup.bpod_path,
             copied_defaults=True,
             details=None,
-            user_config_path=self.bpod_path.joinpath("Config/config.json"),
-            system_config_path=self.system_path.joinpath("config.json"),
+            user_config_path=temp_setup.bpod_path.joinpath("Config/config.json"),
+            system_config_path=temp_setup.system_path.joinpath("config.json"),
         )
         assert result == expected_result
-        assert self.bpod_path.joinpath("Config/config.json").exists()
-        assert self.system_path.joinpath("config.json").exists()
+        assert temp_setup.bpod_path.joinpath("Config/config.json").exists()
+        assert temp_setup.system_path.joinpath("config.json").exists()
 
-    def test_malformed_system_config(self):
+    def test_malformed_system_config(self, temp_setup: TempSetup):
         # Create a malformed system config file
-        self.system_path.mkdir(parents=True, exist_ok=True)
-        malformed_config_path = self.system_path.joinpath("config.json")
+        temp_setup.system_path.mkdir(parents=True, exist_ok=True)
+        malformed_config_path = temp_setup.system_path.joinpath("config.json")
         malformed_config_path.write_text("{ malformed json }")
 
         result = startup.initialize_bpod_system(
             choices=DefaultChoiceAdapter(),
-            default_bpod_path=self.bpod_path,
+            default_bpod_path=temp_setup.bpod_path,
             logger=get_logger(),
         )
 
         assert result.state == startup.InitState.FAILED
         # assert "Malformed system config" in result.message  # fails
 
-    def test_log_swap_stream(self):
+    def test_log_swap_stream(self, temp_setup: TempSetup):
         # Test that the logger's stream is swapped to the file handler
         logger = get_logger()
         if logger.file_handler is None or logger.file_handler.log_dir is None:
@@ -112,9 +115,12 @@ class TestInitService:
 
         result = startup.initialize_bpod_system(
             choices=DefaultChoiceAdapter(),
-            default_bpod_path=self.bpod_path,
+            default_bpod_path=temp_setup.bpod_path,
             logger=logger,
         )
 
         assert result.state == startup.InitState.COMPLETED
-        assert logger.file_handler.log_dir == self.bpod_path.joinpath("Logs")
+        assert logger.file_handler.log_dir == temp_setup.bpod_path.joinpath("Logs")
+
+
+class TestHelperFunctions: ...
