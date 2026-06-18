72a05be90252919af17a567bdbc73cada1cafcc4
Austin Pauley <austin2118ace@gmail.com>
2026-06-18 12:18:52 -0400

    cleanup and format tests

--- tests/bpod_rig/managers/test_bpod_manager.py
+++ tests/bpod_rig/managers/test_bpod_manager.py
@@ -1,66 +1,59 @@
-from typing import Generator
+from collections.abc import Generator
 
 import pytest
-from bpod_core.bpod import BpodInfo, discover_bpod
+from bpod_core.bpod import BpodInfo
 
 from bpod_rig.managers import BpodManager
 
 
 def mock_discover_bpod() -> Generator[BpodInfo]:
 
-    bpod_info_1 = BpodInfo(
-        serial_number = "abc123",
-        port = "COM2",
-        name = "Bpod 1"
-    )
+    bpod_info_1 = BpodInfo(serial_number="abc123", port="COM2", name="Bpod 1")
 
     bpod_info_2 = BpodInfo(
-        serial_number = "xyz456",
-        port = "COM3",
-        name = "Bpod 2",
+        serial_number="xyz456",
+        port="COM3",
+        name="Bpod 2",
     )
 
-    for bpod in [bpod_info_1, bpod_info_2]:
-        yield bpod
+    yield from [bpod_info_1, bpod_info_2]
 
 
 class TestBpodManager:
-
     @pytest.fixture(autouse=True)
-    def create_bpm(self, request, monkeypatch):
-        marker = request.node.get_closest_marker("finds_bpods").args[0]
-        print(marker)
+    def create_bpm(
+        self, request: pytest.FixtureRequest, monkeypatch: pytest.MonkeyPatch
+    ) -> None:
+        marker = request.node.get_closest_marker("finds_bpods")
         if not marker:
             monkeypatch.setattr("bpod_rig.managers.bpod_manager.discover_bpod", list)
         else:
             monkeypatch.setattr(
                 "bpod_rig.managers.bpod_manager.discover_bpod", mock_discover_bpod
-                )
+            )
 
         self.bpm = BpodManager()
-        self.bpm.load()
+        self.bpm.refresh()
 
-
-    @pytest.mark.finds_bpods(False)
-    def test_discover_no_bpods(self, caplog):
-        self.bpm.load()
+    def test_discover_no_bpods(self, caplog: pytest.LogCaptureFixture) -> None:
+        self.bpm.refresh()
         assert "No local Bpods found!" in caplog.messages
         assert self.bpm._all_local_bpods is None
         assert self.bpm._get_local_bpods() is None
 
-    @pytest.mark.finds_bpods(True)
-    def test_discover_bpods(self):
+    @pytest.mark.finds_bpods
+    def test_discover_bpods(self) -> None:
         assert self.bpm._all_local_bpods is not None
         assert list(self.bpm._all_local_bpods.keys()) == ["abc123", "xyz456"]
         assert self.bpm._get_local_bpods() is self.bpm._all_local_bpods
 
-    @pytest.mark.finds_bpods(False)
-    def test_select_bpod_no_bpods(self):
+    # @pytest.mark.finds_bpods(False)
+    def test_select_bpod_no_bpods(self) -> None:
         with pytest.raises(ValueError):
             self.bpm.select_bpod()
 
-    @pytest.mark.finds_bpods(True)
-    def test_select_bpod(self):
+    @pytest.mark.finds_bpods
+    def test_select_bpod(self) -> None:
 
         assert self.bpm._all_local_bpods is not None
 
@@ -71,7 +64,7 @@ class TestBpodManager:
         assert self.bpm.current_bpod.port == "COM2"
         assert self.bpm.current_bpod.name == "Bpod 1"
 
-        self.bpm.select_bpod(serial = "xyz456")
+        self.bpm.select_bpod(serial="xyz456")
         assert self.bpm.current_bpod is self.bpm._all_local_bpods["xyz456"]
         assert self.bpm.current_bpod.serial_number == "xyz456"
         assert self.bpm.current_bpod.port == "COM3"
@@ -83,8 +76,8 @@ class TestBpodManager:
         assert self.bpm.current_bpod.port == "COM3"
         assert self.bpm.current_bpod.name == "Bpod 2"
 
-    @pytest.mark.finds_bpods(True)
-    def test_select_bpod_errors(self):
+    @pytest.mark.finds_bpods
+    def test_select_bpod_errors(self) -> None:
         with pytest.raises(KeyError):
             self.bpm.select_bpod(serial="bad_serial")
 
