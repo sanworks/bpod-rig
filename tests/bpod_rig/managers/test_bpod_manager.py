7b673f85204987c864c391b910073e56fa6fcc15
Austin Pauley <austin2118ace@gmail.com>
2026-06-28 21:36:51 -0400

    bpod_manager unit tests

--- tests/bpod_rig/managers/test_bpod_manager.py
+++ tests/bpod_rig/managers/test_bpod_manager.py
@@ -5,18 +5,30 @@ from bpod_core.bpod.structs import BpodInfo
 
 from bpod_rig.managers import BpodManager
 
+bpod_info_1 = BpodInfo(
+    serial_number="abc123",
+    port="COM2",
+    name="Bpod 1"
+)
 
-def mock_discover_bpod() -> Generator[BpodInfo]:
+bpod_info_2 = BpodInfo(
+    serial_number="xyz456",
+    port="COM3",
+    name="Bpod 2",
+)
 
-    bpod_info_1 = BpodInfo(serial_number="abc123", port="COM2", name="Bpod 1")
+def mock_discover_bpod(num_bpods: int = 2) -> Generator[BpodInfo]:
+    if num_bpods > 2:
+        raise IndexError("Too many Bpods requested!")
 
-    bpod_info_2 = BpodInfo(
-        serial_number="xyz456",
-        port="COM3",
-        name="Bpod 2",
-    )
+    yield from [bpod_info_1, bpod_info_2][:num_bpods]
 
-    yield from [bpod_info_1, bpod_info_2]
+
+def compare_bpod_info(selected_bpod: BpodInfo, expected_bpod: BpodInfo) -> None:
+    assert selected_bpod.serial_number == expected_bpod.serial_number
+    assert selected_bpod.name == expected_bpod.name
+    assert selected_bpod.port == expected_bpod.port
+    assert selected_bpod is expected_bpod
 
 
 class TestBpodManager:
@@ -28,8 +40,10 @@ class TestBpodManager:
         if not marker:
             monkeypatch.setattr("bpod_rig.managers.bpod_manager.discover_bpod", list)
         else:
+            num_bpods = marker.args[0]
             monkeypatch.setattr(
-                "bpod_rig.managers.bpod_manager.discover_bpod", mock_discover_bpod
+                "bpod_rig.managers.bpod_manager.discover_bpod",
+                lambda: mock_discover_bpod(num_bpods)
             )
 
         self.bpm = BpodManager()
@@ -41,52 +55,80 @@ class TestBpodManager:
         assert self.bpm._all_local_bpods is None
         assert self.bpm._get_local_bpods() is None
 
-    @pytest.mark.finds_bpods
+    @pytest.mark.finds_bpods(2)
     def test_discover_bpods(self) -> None:
         assert self.bpm._all_local_bpods is not None
         assert list(self.bpm._all_local_bpods.keys()) == ["abc123", "xyz456"]
+        # Is the value returned correctly?
         assert self.bpm._get_local_bpods() is self.bpm._all_local_bpods
 
-    # @pytest.mark.finds_bpods(False)
     def test_select_bpod_no_bpods(self) -> None:
         with pytest.raises(ValueError):
             self.bpm.select_bpod()
 
-    @pytest.mark.finds_bpods
-    def test_select_bpod(self) -> None:
+    @pytest.mark.finds_bpods(1)
+    def test_select_one_bpod(self) -> None:
 
-        assert self.bpm._all_local_bpods is not None
-
-        current_bpod = self.bpm.select_bpod()
-        assert current_bpod is not None
-
+        # Test No Parameters
+        returned_bpod = self.bpm.select_bpod()
         assert self.bpm.current_bpod is not None
-        assert self.bpm.current_bpod is current_bpod
-        assert self.bpm.current_bpod is self.bpm._all_local_bpods["abc123"]
-        assert self.bpm.current_bpod is not self.bpm._all_local_bpods["xyz456"]
-        assert self.bpm.current_bpod.port == "COM2"
-        assert self.bpm.current_bpod.name == "Bpod 1"
+        compare_bpod_info(self.bpm.current_bpod, bpod_info_1)
+        compare_bpod_info(returned_bpod, bpod_info_1)
 
-        self.bpm.select_bpod(serial="xyz456")
+        # Reset current bpod and test select by index
+        self.bpm.current_bpod = None
+        returned_bpod = self.bpm.select_bpod(index=0)
         assert self.bpm.current_bpod is not None
-        assert self.bpm.current_bpod is self.bpm._all_local_bpods["xyz456"]
-        assert self.bpm.current_bpod.serial_number == "xyz456"
-        assert self.bpm.current_bpod.port == "COM3"
-        assert self.bpm.current_bpod.name == "Bpod 2"
+        compare_bpod_info(self.bpm.current_bpod, bpod_info_1)
+        compare_bpod_info(returned_bpod, bpod_info_1)
 
-        self.bpm.select_bpod(index=1)
-        assert self.bpm.current_bpod is self.bpm._all_local_bpods["xyz456"]
-        assert self.bpm.current_bpod.serial_number == "xyz456"
-        assert self.bpm.current_bpod.port == "COM3"
-        assert self.bpm.current_bpod.name == "Bpod 2"
+        # Reset current bpod and test select by serial
+        self.bpm.current_bpod = None
+        returned_bpod = self.bpm.select_bpod(serial="abc123")
+        assert self.bpm.current_bpod is not None
+        compare_bpod_info(self.bpm.current_bpod, bpod_info_1)
+        compare_bpod_info(returned_bpod, bpod_info_1)
 
-    @pytest.mark.finds_bpods
-    def test_select_bpod_errors(self) -> None:
-        with pytest.raises(KeyError):
+
+    @pytest.mark.finds_bpods(2)
+    def test_select_multiple_bpods(self) -> None:
+
+        returned_bpod = self.bpm.select_bpod(serial="abc123")
+        assert self.bpm.current_bpod is not None
+        compare_bpod_info(self.bpm.current_bpod, bpod_info_1)
+        compare_bpod_info(returned_bpod, bpod_info_1)
+
+        returned_bpod = self.bpm.select_bpod(serial="xyz456")
+        assert self.bpm.current_bpod is not None
+        compare_bpod_info(self.bpm.current_bpod, bpod_info_2)
+        compare_bpod_info(returned_bpod, bpod_info_2)
+
+        returned_bpod = self.bpm.select_bpod(index=0)
+        assert self.bpm.current_bpod is not None
+        compare_bpod_info(self.bpm.current_bpod, bpod_info_1)
+        compare_bpod_info(returned_bpod, bpod_info_1)
+
+        returned_bpod = self.bpm.select_bpod(index=1)
+        assert self.bpm.current_bpod is not None
+        compare_bpod_info(self.bpm.current_bpod, bpod_info_2)
+        compare_bpod_info(returned_bpod, bpod_info_2)
+
+
+    @pytest.mark.finds_bpods(2)
+    def test_select_bpod_errors(self, caplog: pytest.LogCaptureFixture) -> None:
+        with pytest.raises(ValueError):
+            # Parameters are required with multiple Bpods
+            self.bpm.select_bpod()
+            assert "A serial number or index is required!" in caplog.messages
+
+        with pytest.raises(ValueError):
             self.bpm.select_bpod(serial="bad_serial")
+            assert "not found" in caplog.messages
 
         with pytest.raises(IndexError):
             self.bpm.select_bpod(index=99)
+            assert "out of range" in caplog.messages
 
         with pytest.raises(IndexError):
             self.bpm.select_bpod(index=-99)
+            assert "out of range" in caplog.messages
